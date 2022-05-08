2c8c84e509192b3a74706b39c0a62154c337f7e6
Shreyashi <shreyashi04@gmail.com>
2022-05-08 22:23:10 +0200

    Adding validation for email step and showing error message for invalid input

--- src/buyflow/emailStep/EmailStep.tsx
+++ src/buyflow/emailStep/EmailStep.tsx
@@ -3,9 +3,19 @@ import React, { useState } from 'react'
 interface EmailStepProps {
   cb: (field: string, value: string) => void
 }
+const re = /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/
 
 const EmailStep: React.FC<EmailStepProps> = (props) => {
   const [email, setEmail] = useState('')
+  const [emailErr, setEmailErr] = useState(false);
+
+  const isValidEmail = (): boolean => re.test(email)
+
+  function onClickHandler():any{
+    const validEmail = isValidEmail() 
+    validEmail ? props.cb('email', email) : setEmailErr(true)
+  }
+
   return (
     <>
       <div>
@@ -14,11 +24,14 @@ const EmailStep: React.FC<EmailStepProps> = (props) => {
           type="email"
           onChange={({ target: { value } }) => {
             setEmail(value)
+            setEmailErr(false)
           }}
           value={email}
         ></input>
       </div>
-      <button onClick={() => props.cb('email', email)}>Next</button>
+      {emailErr && <p> Email is invalid</p>}
+      <button onClick={onClickHandler}>Next</button>
+
     </>
   )
 }
