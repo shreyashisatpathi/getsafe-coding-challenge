1fd045f17d054017fadcdf6250cca88d03f01131
Shreyashi <shreyashi04@gmail.com>
2022-05-09 01:23:34 +0200

    adding summary step

--- src/buyflow/SummaryStep.tsx
+++ src/buyflow/SummaryStep.tsx
@@ -5,14 +5,17 @@ interface SummaryStepProps {
   collectedData: {
     email: string
     age: number
+    name : string
   }
 }
 
 const SummaryStep: React.FC<SummaryStepProps> = (props) => {
+  const {email, age, name} = props.collectedData
   return (
     <>
-      <div>Email: {props.collectedData.email}</div>
-      <div>Age: {props.collectedData.age}</div>
+      <div>Email: {email}</div>
+      <div>Age: {age}</div>
+      {name && <div>Name: {name}</div>}
       <div>
         <Link to="/purchased=dev_ins">Purchase</Link>
       </div>
