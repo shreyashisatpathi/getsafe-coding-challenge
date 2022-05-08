43ac6c2d99783e56933c9c52dc548012d7e26eaa
Shreyashi <shreyashi04@gmail.com>
2022-05-08 23:42:35 +0200

    Moving age step into its folder

--- src/buyflow/ageStep/AgeStep.test.tsx
+++ src/buyflow/ageStep/AgeStep.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import AgeStep from './AgeStep'
 import userEvent from '@testing-library/user-event'
 
